98c3dbaea6f45e26cb9187c835eb76d3e13b4070
mifta08 <rizqhaamifta@gmail.com>
2025-06-09 00:49:28 +0700

    menambahkan route dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, useLocation, Routes } from 'react-router'
 import './assets/css/main.css'
 import Login from './page/Login';
+import Dashboard from './page/Dashboard';
 
 function App() {
   const location = useLocation();
   return (
     <Routes key={location.pathname} location={location}>
       <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   )
 }
