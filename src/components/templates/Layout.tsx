619c9cd37a7de53aa352aba8804caf248dc12886
mifta08 <rizqhaamifta@gmail.com>
2025-06-09 01:56:13 +0700

    menambahkan komponen sidebar

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,15 +1,12 @@
-const Layout = ({
-    className,
-    children
-}) => {
-    return (
-        <div className="layout h-full grid grid-cols-[15%_auto] bg-[#2A0E99]">
-            <aside className="bg-white h-full p-5">Sidebar</aside>
-            <main className={`h-full p-10 ${className}`}>
-                {children}
-            </main>
-        </div>
-    )
-}
+import Sidebar from "../molecules/Sidebar";
+
+const Layout = ({ className, children }) => {
+  return (
+    <div className="layout h-full grid grid-cols-[15%_auto] bg-[#2A0E99]">
+      <Sidebar />
+      <main className={`h-full p-10 ${className}`}>{children}</main>
+    </div>
+  );
+};
 
 export default Layout;
