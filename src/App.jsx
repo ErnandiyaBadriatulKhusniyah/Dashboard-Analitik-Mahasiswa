932cb444e2832548cb609c03347817dbd8160f7f
mifta08 <rizqhaamifta@gmail.com>
2025-06-09 01:06:02 +0700

    menambahkan route untuk mahasiswa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, useLocation, Routes } from 'react-router'
 import './assets/css/main.css'
 import Login from './page/Login';
+import Mahasiswa from './page/Mahasiswa';
 
 function App() {
   const location = useLocation();
   return (
     <Routes key={location.pathname} location={location}>
       <Route path='/login' element={<Login />} />
+      <Route path='/mahasiswa' element={<Mahasiswa />} />
     </Routes>
   )
 }
 
-export default App
+export default App;
