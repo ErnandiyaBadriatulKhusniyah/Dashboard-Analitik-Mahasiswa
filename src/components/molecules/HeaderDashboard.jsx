3a797646a76b5685f28d32a5d2b3ac7799de7b82
mifta08 <rizqhaamifta@gmail.com>
2025-06-09 01:55:25 +0700

    menambahkan children props

--- src/components/molecules/HeaderDashboard.jsx
+++ src/components/molecules/HeaderDashboard.jsx
@@ -1,10 +1,10 @@
 import Typography from "../atoms/Typograpy";
 
-const HeaderDashboard = () => {
+const HeaderDashboard = ({ children }) => {
     return (
         <div className="flex items-center justify-between bg-white p-4 rounded-lg">
             <Typography variantClass="h1" className="font-semibold">
-                Home
+                {children}
             </Typography>
             <Typography>Hi, Miftahul Rizqha</Typography>
         </div>
