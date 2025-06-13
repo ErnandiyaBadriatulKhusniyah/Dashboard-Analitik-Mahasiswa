2949cd7d5438dffb4c301b75acfd2411807e39df
--list <rizqhaamifta@gmail.com>
2025-06-13 20:26:51 +0700

    mengubah kode di sidebar

--- src/components/molecules/Sidebar.jsx
+++ src/components/molecules/Sidebar.jsx
@@ -1,5 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
 
+import IconSI from "../../assets/icon/Prodi.png";
+import { House } from 'lucide-react';
+import { GraduationCap } from 'lucide-react';
+import { FileCheck } from 'lucide-react';
+import { ChartNoAxesColumn } from 'lucide-react';
+import { LogOut } from 'lucide-react';
+
 const Sidebar = () => {
 
     const location = useLocation(); // Untuk tahu URL aktif yaww miww
@@ -10,35 +17,35 @@ const Sidebar = () => {
         <div className="w-60 bg-white text-black p-4 flex flex-col rounded-r-2xl justify-between">
             <div>
                 <div className="flex justify-center mb-6 p-10">
-                    <img src="icon/LoginIcon.webp" alt="logo" className="h-25 w-26" />
+                    <img src={IconSI} alt="logo" className="h-17 w-23" />
                 </div>
 
                 <section className="space-y-4 flex flex-col">
 
                     <Link to="/dashboard" className={`flex flex-row gap-5 ${isActive("/dashboard") ? "text-[#1D0080] font-bold" : "text-gray-400"}`}>
-                        <img src="icon/home.png" className={`${isActive("/dashboard") ? "" : "grayscale"}`} />
+                        <House />
                         <span>Home</span>
                     </Link>
 
                     <Link to="/mahasiswa" className={`flex flex-row gap-5 ${isActive("/mahasiswa") ? "text-[#1D0080] font-bold" : "text-gray-400"}`}>
-                        <img src="icon/bachlor.png" className={`${isActive("/mahasiswa") ? "" : "grayscale"}`} />
+                        <GraduationCap />
                         <span>Data Mahasiswa</span>
                     </Link>
 
                     <Link to="/input" className={`flex flex-row gap-5 ${isActive("/input") ? "text-[#1D0080] font-bold" : "text-gray-400"}`}>
-                        <img src="icon/input.png" className={`${isActive("/input") ? "" : "grayscale"}`} />
+                        <FileCheck />
                         <span>Input & Evaluasi</span>
                     </Link>
 
                     <Link to="/statistik" className={`flex flex-row gap-5 ${isActive("/statistik") ? "text-[#1D0080] font-bold" : "text-gray-400"}`}>
-                        <img src="icon/bar.png" className={`${isActive("/statistik") ? "" : "grayscale"}`} />
+                        <ChartNoAxesColumn />
                         <span>Statistik Program Studi</span>
                     </Link>
                 </section>
             </div>
 
-            <div className="flex flex-row gap-5">
-                <img src="icon/out.png" alt="" />
+            <div className="flex flex-row gap-5 text-[#1D0080] font-bold ">
+                <LogOut />
                 <a href="#">Logout</a>
             </div>
         </div>
