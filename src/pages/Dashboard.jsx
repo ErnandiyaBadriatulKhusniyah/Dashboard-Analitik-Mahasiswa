9c70d3cbd9386d1d811786dc2bbe92fb35135fb4
mifta08 <rizqhaamifta@gmail.com>
2025-06-22 18:34:06 +0700

    mengubah nama file page menjadi pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -29,17 +29,18 @@ const matakuliah = [
     },
 
 ];
+const chartColors = [
+    '#7893FF',
+    '#573BD4',
+    '#2B4386',
+    '#7887B9',
+    '#A79BD7',
+];
 const data = {
     labels: matakuliah.map(item => item.mk),
     datasets: [{
         data: matakuliah.map(item => item.total_mahasiswa),
-        backgroundColor: [
-            '#7893FF',
-            '#573BD4',
-            '#2B4386',
-            '#7887B9',
-            '#A79BD7',
-        ]
+        backgroundColor: chartColors
     }],
 };
 
@@ -57,7 +58,7 @@ export const options = {
             align: 'start'
         },
         legend: { // Atur posisi legend jika perlu
-            position: 'left',
+            position: 'bottom',
             align: 'center',
         },
     },
@@ -68,32 +69,35 @@ const Dashboard = () => {
         <main className="h-screen w-full">
             <Layout className="flex flex-col gap-10 ">
                 <HeaderDashboard children={"Dashboard"} />
-                <section className="bg-white rounded-lg py-4 px-10 space-y-5">
-                    <Typography variantClass="h1" className="font-bold text-center text-2xl">Top Matakuliah Pilihan Mahasiswa</Typography>
-                    <div className="grid grid-cols-[50%_50%] justify-center">
-                        <div className="">
+                <section className=" space-y-5">
+                    <div className="justify-center grid grid-cols-[50%_50%] space-x-5">
+                        <div className="bg-white rounded-lg py-4 px-10">
                             <Doughnut options={options} className="max-h-[300px] max-w-fit" data={data} />
                         </div>
-                        <div className="flex flex-col gap-2">
-                            <div className="bg-[#F2F2F2] grid grid-cols-2 text-center p-1 rounded-md shadow">
-                                <Typography variantClass="h4">
+                        <div className="flex flex-col gap-2 bg-white rounded-lg py-4 px-10">
+                            <div className="grid grid-cols-2 pl-4">
+                                <Typography variantClass="h4" className={'font-semibold'}>
                                     Mata Kuliah
                                 </Typography>
-                                <Typography variantClass="h4">
+                                <Typography variantClass="h4" className={'font-semibold'}>
                                     Total Mahasiswa
                                 </Typography>
                             </div>
-                            <div>
-                                {matakuliah.map((item, index) => (
-                                    <div key={index} className="grid grid-cols-2 text-center p-1 rounded-md ">
-                                        <Typography variantClass="p" className="text-black">
-                                            {item.mk}
-                                        </Typography>
-                                        <Typography variantClass="p">
-                                            {item.total_mahasiswa}
-                                        </Typography>
-                                    </div>
-                                ))}
+                            <div className="">
+                                {matakuliah.map((item, index) => {
+                                    const borderColor = chartColors[index % chartColors.length];
+                                    return (
+                                        <div key={index} className={`grid grid-cols-2 border-b pl-4 py-3 border-t border-gray-200 border-l-4`}
+                                            style={{ borderLeftColor: borderColor }}>
+                                            <Typography variantClass="p" className="text-black">
+                                                {item.mk}
+                                            </Typography>
+                                            <Typography variantClass="p">
+                                                {item.total_mahasiswa}
+                                            </Typography>
+                                        </div>
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
